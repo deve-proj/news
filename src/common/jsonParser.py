1cdcb6e1fc18435032cef8dc8f42cb46f39da7cf
Neo <lovchikovmmark@gmail.com>
2026-03-10 17:22:03 +0300

    fix link generator

--- src/common/jsonParser.py
+++ src/common/jsonParser.py
@@ -9,6 +9,6 @@ def replace_image_name_to_url_in_post(post : create_post_model, post_id : str = 
 
     for image in list(filter(lambda block: block.type == 'image', post.post.content)):
 
-        image.value = f'http://{os.getenv("MINIO_HOST")}{f":{os.getenv("MINIO_PORT") if os.getenv("MINIO_HOST") == 'localhost' else ""}"}/posts/{post_id}/{image.value}'
+        image.value = f'http://{os.getenv("MINIO_HOST")}{f":{os.getenv("MINIO_PORT")}" if os.getenv("MINIO_HOST") == 'localhost' else ''}/posts/{post_id}/{image.value}'
 
     return post.post.model_dump()
